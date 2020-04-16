aba15100abf374dd8bea3365a0f3f22064ff3df7
sagnikkar <36636592+sagnikkar@users.noreply.github.com>
2020-04-16 15:40:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def predict():
         #columns  -- Age,	Fever,	BodyPains,	RunnyNose,	Difficulty_in_Breath
         data = [[int(age),int(fever),int(body),int(cold),int(breath)]]
         predict = clf.predict(data)[0]
-        proba_score = clf.predict_proba(data)[0][0]
+        proba_score = clf.predict_proba(data)
         
         if predict==1:
             prediction='Positive'
